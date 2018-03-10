4aed14b1a0c9cef1652c59f44b9abc93c4b38bd5
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-10 14:41:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT category, SUM(amount)
 FROM projects
 INNER JOIN pledges
-on project.id = pledges.project_id
+ON projects.id = pledges.project_id
 WHERE category = 'music';"
 end
 
