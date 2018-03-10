3d9b775a5bc44166fe2670171d994b205a085d8d
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-10 14:28:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ ORDER BY name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title, SUM(amount)
+"SELECT title, SUM(amount)-funding_goal
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
